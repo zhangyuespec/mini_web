1850ec5024e8aad08ed5e67d47cc9c84dfadd422
zhangyue <846642539@qq.com>
2018-11-30 18:13:41 +0800

    接耦合

---
+++ mini_web框架/04-将web服务器和逻辑处理的代码分开--升级版/test.py
@@ -0,0 +1,20 @@
+import time
+
+def login():
+    return "welcome to our website ----time is %s "%time.time()
+
+def register():
+    return "登录"
+
+def profile():
+    return "个人主页"
+
+def application(file_name):
+    if file_name=="log_in.py":
+        login()
+    elif file_name=="register.py":
+        register()
+    elif file_name=="profile":
+        profile()
+    else:
+        return "没有发现你的主页"
