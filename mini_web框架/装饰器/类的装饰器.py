83648c6c0bcafa88797123fd69067d347bd6f876
zhangyue <846642539@qq.com>
2018-12-03 21:31:21 +0800

    类的装饰器（爬虫）

--- mini_web框架/装饰器/类的装饰器.py
+++ mini_web框架/装饰器/类的装饰器.py
@@ -1,13 +1,11 @@
-class Test(object):
-    def __init__(self,func):
-        self.func=func
-
-    def __call__(self, *args, **kwargs):
-        print("这里是装饰器添加的功能")
-        return self.func()
-
-@Test
-def get_str():
-    return "zhadf"
-
-print(get_str())
+import requests
+from lxml import html
+url='https://music.douban.com/' #需要爬的网址
+page=requests.Session().get(url) 
+tree=html.fromstring(page.text) 
+result=tree.xpath('//tr//a/text()') #需要获取的数据
+result1=tree.xpath('//tr//a/@href')
+result2=tree.xpath('//tr[last()]//a/@href')
+print(result)
+print(result1)
+print(result2)
