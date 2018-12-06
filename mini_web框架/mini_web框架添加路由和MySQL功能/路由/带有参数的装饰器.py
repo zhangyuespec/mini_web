59a66cb7a80a86892a9d2c63a011e70e062673c0
zhangyue <846642539@qq.com>
2018-12-06 12:19:28 +0800

    .

--- mini_web框架/mini_web框架添加路由和MySQL功能/路由/带有参数的装饰器.py
+++ mini_web框架/mini_web框架添加路由和MySQL功能/路由/带有参数的装饰器.py
@@ -1,1 +1,29 @@
-import
+def set_level(level_num):
+    def set_func(func):
+        def call_func(*args, **kwargs):
+            # level = args[0]
+            if level_num == 1:
+                print("权限验证１")
+            elif level_num == 2:
+                print("权限验证２")
+            return func()
+
+        return call_func
+
+    return set_func
+
+
+@set_level(1)
+def test1():
+    print("test1")
+    return "OK"
+
+
+@set_level(2)
+def test2():
+    print("test2")
+    return "OK"
+
+
+test1()
+test2()
